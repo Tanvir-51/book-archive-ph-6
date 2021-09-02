9f9ec31a4504f030ec7d6aea7c26c1dbf078b933
Tanvir-51 <tanvir19ahmed@gmail.com>
2021-09-02 20:27:27 +0600

    'updated'

--- js/script.js
+++ js/script.js
@@ -6,8 +6,43 @@ const searchBook = () => {
   searchInput.value = "";
 
   const url = `https://openlibrary.org/search.json?q=${searchText}`;
-  console.log(url);
-  //   fetch(url)
-  //     .then((res) => res.json())
-  //     .then((data) => console.log(data));
+  //   console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayBookResult(data.docs));
+};
+
+const displayBookResult = (books) => {
+  const searchResult = document.getElementById("search-result");
+  books.forEach((book) => {
+    console.log(book);
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+        <div class="card h-100">
+          <img src="https://covers.openlibrary.org/b/id/${
+            book.cover_i
+          }-M.jpg" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Name: ${book.title}</h5>
+            <p class="card-text">Writer: ${
+              book.author_name === undefined
+                ? "It's undefined"
+                : book.author_name[0]
+            }</p>
+            <p class="card-text">Publisher: ${
+              book.publisher === undefined
+                ? "It's undefined"
+                : book.publisher[0]
+            }</p>
+            <p class="card-text">published: ${
+              book.first_publish_year === undefined
+                ? "It's undefined"
+                : book.first_publish_year
+            }</p>
+          </div>
+        </div>
+    `;
+    searchResult.appendChild(div);
+  });
 };
